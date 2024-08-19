3a825bb67267c33ef945eb17b93823d8b473ed3e
goodmanzeng <sandaokongjian@gmail.com>
2024-08-19 13:25:37 +0800

    Site updated: 2024-08-19 13:25:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/19/第一篇测试/","2024/08/19/hello-world/"];function toRandomPost(){
+var posts=["article/第一篇测试/","article/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
